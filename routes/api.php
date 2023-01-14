536b834600226eb8894d8f0c75479de62bd7e186
Milosz3214 <mw305421@student.polsl.pl>
2023-01-14 17:28:21 +0100

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PeopleController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('wcislinski/305421/people',[PeopleController::class, 'index']);
+Route::get('wcislinski/305421/people/{people}',[PeopleController::class, 'show']);
+Route::put('wcislinski/305421/people/{people}',[PeopleController::class, 'update']);
+Route::delete('wcislinski/305421/people/{people}',[PeopleController::class, 'delete']);
+Route::post('wcislinski/305421/people',[PeopleController::class, 'create']);
